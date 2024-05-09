7007e6cf373a0f18e4b8ec3841c4479b596f9959
Khushi Kadyan <kadyankhushi290@gmail.com>
2024-05-09 12:25:17 +0530

    js is commited finally

--- project/calci/calci.js
+++ project/calci/calci.js
@@ -1,4 +1,33 @@
 const display= document.querySelector(".display");
 const buttons= document.querySelectorAll("button");
 
-console.log(display,buttons);
+const specialchars =["%","*","/","-","+","="];
+let output="";
+
+//define function to calculate based on button
+const calculate =(btnValue) => {
+    if(btnValue ==="=" && output !==""){
+        //if outpu has "% ,replace with '/100' before evaluating.
+        output = eval(output.replace("%","/100"));
+    }
+    else if(btnValue==="AC"){
+        output= "";
+    }
+    else if(btnValue==="DEL"){
+        //if del button is pressed remove the last character from the output.
+        output = output.slice(0,-1);//.toString().slice....  
+    }
+    else{
+        //if output is empty and buttton is specialChars then return
+        if(output=="" && specialchars.includes(btnValue)) return;
+        output += btnValue;
+      
+    }
+    display.value = output;
+};
+
+//add event listner to buttons,call calculator( on click)
+buttons.forEach((button) => {
+    //button click listner calls calculator() with dataset value as argument.
+    button.addEventListener("click",e => calculate(e.target.dataset.value))
+});
