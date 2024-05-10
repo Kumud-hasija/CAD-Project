314bc98c892d8e494c7782fe6f4f2b55eac3374e
Khushi Kadyan <kadyankhushi290@gmail.com>
2024-05-10 16:57:31 +0530

    done

--- maths_play/xyz.js
+++ maths_play/xyz.js
@@ -95,3 +95,21 @@ document.addEventListener('DOMContentLoaded', function() {
   // Add similar event listeners for other conversions
 
 });
+/*formula section */
+document.addEventListener('DOMContentLoaded', function() {
+    const collapsibleButtons = document.querySelectorAll('.collapsible');
+  
+    collapsibleButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            this.classList.toggle('active');
+            const content = this.nextElementSibling;
+            if (content.style.display === 'block') {
+                content.style.display = 'none';
+            } else {
+                content.style.display = 'block';
+            }
+        });
+    });
+  });
+  
+  
